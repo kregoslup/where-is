b4b74420a2343206a1c0c8e4269bc7db9d5b75f8
krego <mateusz.olczynski@gmail.com>
2017-03-28 20:18:48 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='where-is',
+    version='0.1',
+    description='Tool for searching for configuration files and logs',
+    url='https://github.com/kregoslup/where-is',
+    author='kregoslup',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+    license='MIT',
+    packages=['where']
+)
